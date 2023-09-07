897f61ab8282fe3eef955b2cf3c93d819614f55f
Mijail Ziskevich <zismija@gmail.com>
2023-09-07 15:44:36 -0300

    eventos, storage y Json agrega2

--- js/presupuesto.js
+++ js/presupuesto.js
@@ -1,31 +1,3 @@
-/* alert("Calculá tu presupuesto!!")
-
-// Pongo "Murcielago" sin tilde para que no sea tan complicado y no haya confuciones a la hora de darle valor al prompt
-let problematica = prompt("¿Cuál es tu problemática? \n -Roedores \n -Cucarachas \n -Murcielagos \n -Palomas");
-let valor = 0;
-
-if (problematica == "Roedores") {
-    valor = "$5mil";
-}else if (problematica == "Cucarachas") {
-    valor = "$8mil";
-}else if (problematica == "Murcielagos") {
-    valor = "$10mil";
-}else if (problematica == "Palomas") {
-    valor = "$13mil";
-}else {
-    valor = 0;
-}
-
-
-if (valor != 0) {
-    alert ("Tu problema es de " + problematica + ", por lo que el precio del producto será de: " + valor)
-}else {
-    alert ("No elegiste una opción válida, intentá denuevo (si elegiste una de las problemáticas que aparecen entre las opciones, verificá que hayas escrito con mayúscula la primer letra)")
-}
-
-// Próximamente voy a hacer que se puedan sumar problemáticas (plagas) y que el precio final del presupesto se deba al resultado de la suma de otras cosas además de la "problematica"
- */
-
 // Hago uso de los objetos dentro de un array para definir que tipos de tratamiento tengo :D
 const problematica = [
     {nombre: "Roedores", valor: 60},
@@ -46,8 +18,12 @@ for (const nombre of problematica) {
     console.log(nombre);
 }
 
+// Agregué éste evento para que todo el presupuesto se calcule después de que el cliente rellene el formulario y lo envíe
+const boton = document.querySelector ("button.botonForm")
+boton.addEventListener("click", () =>{
+
 // Acá hago un prompt parseado para que el cliente elija cual de los problemas tiene 
-let seleccionDeProblematica = parseInt(prompt("Seleccioná tu problemática:\n1. Roedores\n2. Cucarachas\n3. Murcielagos\n4. Palomas"));
+const seleccionDeProblematica = parseInt(prompt("Seleccioná tu problemática:\n1. Roedores\n2. Cucarachas\n3. Murcielagos\n4. Palomas"));
 
 // Uso un if para determinar si la problemática del usuario es válida, o sea que si el numeor que el cliente elije es menor a 1 o mayor a la cantidad de objetos dentro del array (4), le enviará una alerta de que la opción es invlida. Lo mismo pasa si el carácter ingrasaddo no es un número 
 if (isNaN(seleccionDeProblematica) || seleccionDeProblematica < 1 || seleccionDeProblematica > problematica.length) {
@@ -84,3 +60,29 @@ if (isNaN(metrosCuadrados) || metrosCuadrados <= 0) {
     }
 }
 }
+});
+
+
+const formulario = document.querySelector("form.formularioProblema");
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const nombreCliente = e.target[0].value;
+    const correoCliente = e.target[1].value;
+    const telefonoCliente = e.target[2].value;
+    const problemaCliente = e.target[3].value;
+
+    const respuestas = {
+        nombre: nombreCliente,
+        correo: correoCliente,
+        telefono: telefonoCliente,
+        direccion: problemaCliente,
+};
+
+
+    localStorage.setItem("Cliente", JSON.stringify(respuestas));
+
+});
+
+
+// PD: me costó mucho añadir evento y cuando lo logré me di cuenta que era una pavada xD
